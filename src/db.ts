8693f6903f0c9d319c4c3269ac557915cd0cb90a
Jeong Quokka <107650362+chipmunk-dev@users.noreply.github.com>
2024-07-14 00:54:15 +0900

    #1 Set database (#2)
    - database with typeorm
    - redis with typeorm
    - set docker-compose

---
+++ src/db.ts
@@ -0,0 +1,19 @@
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { env } from './const/env';
+
+type DatabaseType = 'mysql' | 'postgres' | 'mongodb';
+
+const AppDataSource = new DataSource({
+  type: env.DB_TYPE as DatabaseType,
+  host: env.DB_HOST,
+  port: env.DB_PORT,
+  username: env.DB_USERNAME,
+  password: env.DB_PASSWORD,
+  database: env.DB_DATABASE,
+  entities: [],
+  synchronize: true,
+  logging: false,
+});
+
+export default AppDataSource;
---
+++ src/redis.ts
@@ -0,0 +1,14 @@
+import Redis from 'ioredis';
+import { env } from './const/env';
+
+const redisClient = new Redis(env.REDIS_HOST);
+
+redisClient.on('connect', () => {
+  console.log('Connected Redis...');
+});
+
+redisClient.on('error', (err) => {
+  console.error('Redis connection error:', err);
+});
+
+export default redisClient;
